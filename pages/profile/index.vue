c5b3a934d1b37a3bb1770cdb51af8ea967e50837
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-06-12 12:46:57 -0700

    commit

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -99,15 +99,6 @@
 export default {
   components: {
   },
-  head () {
-    return {
-      title: 'Hello 123',
-      meta: [
-        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-        { hid: 'description', name: 'description', content: 'My custom description' }
-      ]
-    }
-  },
   data() {
     return {
     }
